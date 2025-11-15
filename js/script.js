7526c7fc70f260d4a919c720b952fe766d144f79
Lester Aranca <2240766@slu.edu.ph>
2025-11-15 19:38:35 +0800

    Add map labels w/ hard-coded coords (to be moved to database)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,40 @@
+// Campus map 
+var c = document.getElementById("map-canvas");
+var ctx = c.getContext("2d");
+
+// hard-coded map labels (to be moved to the database)
+ctx.font = "15px Arial";
+ctx.fillText("D501", 48, 40);
+ctx.fillText("D502", 48, 210);
+ctx.fillText("D503", 48, 300);
+ctx.fillText("D504", 48, 390);
+ctx.fillText("D505", 160, 390);
+ctx.fillText("D506", 160, 250);
+ctx.fillText("D507", 160, 160);
+ctx.fillText("D508", 160, 70);
+ctx.fillText("D511", 240, 390);
+ctx.fillText("D512", 320, 390);
+ctx.fillText("D513", 400, 390);
+ctx.fillText("D514", 480, 390);
+ctx.fillText("D515", 560, 390);
+ctx.fillText("D525", 640, 390);
+ctx.fillText("D522", 755, 370);
+ctx.fillText("D524", 755, 230);
+ctx.fillText("D521", 755, 40);
+ctx.fillText("D526", 640, 230);
+ctx.fillText("D528", 640, 90);
+ctx.fillText("CR(M)", 30, 145);
+ctx.fillText("CR(F)", 765, 145);
+
+// Temporary function for help in finding coordinates (to be removed later)
+// Click on map then check console for coordinates
+c.addEventListener('mousedown', (event) => {
+  const rect = c.getBoundingClientRect();
+  const x = event.clientX - rect.left;
+  const y = event.clientY - rect.top;
+  console.log(`Clicked at: x=${x}, y=${y}`);
+});
+
 // Navbar color on scroll
 window.addEventListener("scroll", () => {
   const navbar = document.getElementById("navbar");
