5209ba3832fcec0105d5c2e6023296de3a899fed
erylebds <erylebds@gmail.com>
2025-11-15 15:18:08 +0800

    emergency markers

--- database/getEmergencyRoute.php
+++ database/getEmergencyRoute.php
@@ -5,7 +5,7 @@ $floor_id = isset($_GET['floor_id']) ? intval($_GET['floor_id']) : 5;
 
 $sql = "SELECT room_code, room_type, x_coord, y_coord 
         FROM room 
-        WHERE floor_id = $floor_id AND room_type != 'safe_route'";
+        WHERE floor_id = $floor_id AND room_type = 'safe_route'";
 
 $result = $conn->query($sql);
 
