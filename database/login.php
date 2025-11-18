d7e8cae4bfcdbcb80c82adfddc7e1e82904686c7
Lester Aranca <2240766@slu.edu.ph>
2025-11-18 21:53:28 +0800

    Comment out try login as client (currently unused & may cause error)

--- database/login.php
+++ database/login.php
@@ -34,32 +34,32 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
         }
     }
 
-    // 2) NOT ADMIN, TRY LOGIN AS CLIENT
-    $stmt = $conn->prepare("SELECT * FROM client WHERE username = ? OR email = ?");
-    $stmt->bind_param("ss", $user, $user);
-    $stmt->execute();
-    $result = $stmt->get_result();
+    // // 2) NOT ADMIN, TRY LOGIN AS CLIENT
+    // $stmt = $conn->prepare("SELECT * FROM client WHERE username = ? OR email = ?");
+    // $stmt->bind_param("ss", $user, $user);
+    // $stmt->execute();
+    // $result = $stmt->get_result();
 
-    if ($result->num_rows === 1) {
-        $row = $result->fetch_assoc();
+    // if ($result->num_rows === 1) {
+    //     $row = $result->fetch_assoc();
 
-        if ($pass === $row['password']) {
-            // CLIENT LOGIN SUCCESS
-            $_SESSION['role'] = 'client';
-            $_SESSION['client_id'] = $row['client_id'];
-            $_SESSION['client_username'] = $row['username'];
-            $_SESSION['client_email'] = $row['email'];
+    //     if ($pass === $row['password']) {
+    //         // CLIENT LOGIN SUCCESS
+    //         $_SESSION['role'] = 'client';
+    //         $_SESSION['client_id'] = $row['client_id'];
+    //         $_SESSION['client_username'] = $row['username'];
+    //         $_SESSION['client_email'] = $row['email'];
 
-            echo "<script>
-                    alert('Client login successful');
-                    window.location.href = '../index.html';
-                  </script>";
-            exit();
-        } else {
-            echo "<script>alert('Incorrect password'); window.location.href = '../login.html';</script>";
-            exit();
-        }
-    }
+    //         echo "<script>
+    //                 alert('Client login successful');
+    //                 window.location.href = '../index.html';
+    //               </script>";
+    //         exit();
+    //     } else {
+    //         echo "<script>alert('Incorrect password'); window.location.href = '../login.html';</script>";
+    //         exit();
+    //     }
+    // }
 
     // 3) NOT FOUND IN EITHER TABLE
     echo "<script>alert('Username or email not found'); window.location.href = '../login.html';</script>";
