b2082baf7866bcef3e94a028db409b0d3b85255b
ArvinMarron <2245405@slu.edu.ph>
2025-11-19 11:12:08 +0800

    Update login.php

--- database/login.php
+++ database/login.php
@@ -7,7 +7,7 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
     $user = $_POST['username'];
     $pass = $_POST['password'];
 
-    // 1) TRY LOGIN AS ADMIN
+    // LOGIN AS ADMIN
     $stmt = $conn->prepare("SELECT * FROM admin WHERE username = ? OR email = ?");
     $stmt->bind_param("ss", $user, $user);
     $stmt->execute();
@@ -33,35 +33,6 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
             exit();
         }
     }
-
-    // // 2) NOT ADMIN, TRY LOGIN AS CLIENT
-    // $stmt = $conn->prepare("SELECT * FROM client WHERE username = ? OR email = ?");
-    // $stmt->bind_param("ss", $user, $user);
-    // $stmt->execute();
-    // $result = $stmt->get_result();
-
-    // if ($result->num_rows === 1) {
-    //     $row = $result->fetch_assoc();
-
-    //     if ($pass === $row['password']) {
-    //         // CLIENT LOGIN SUCCESS
-    //         $_SESSION['role'] = 'client';
-    //         $_SESSION['client_id'] = $row['client_id'];
-    //         $_SESSION['client_username'] = $row['username'];
-    //         $_SESSION['client_email'] = $row['email'];
-
-    //         echo "<script>
-    //                 alert('Client login successful');
-    //                 window.location.href = '../index.html';
-    //               </script>";
-    //         exit();
-    //     } else {
-    //         echo "<script>alert('Incorrect password'); window.location.href = '../login.html';</script>";
-    //         exit();
-    //     }
-    // }
-
-    // 3) NOT FOUND IN EITHER TABLE
     echo "<script>alert('Username or email not found'); window.location.href = '../login.html';</script>";
     exit();
 } else {
@@ -70,3 +41,4 @@ if (isset($_POST['username']) && isset($_POST['password'])) {
 }
 
 $conn->close();
+
